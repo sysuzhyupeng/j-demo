c42d0310c3f3367b4f4823512f959c3bb952f874
sysuzhyupeng <673341297@qq.com>
2017-10-17 21:13:30 +0800

    first commit

---
+++ app/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld { // 声明一个公有类:HelloWorld
+	public static void main(String[] args) { // 类中主方法，程序入口点
+		System.out.println("Hello World !");
+		// 在屏幕上输出字符串"Hello World !" 
+	}
+}
